e32467818c51eac6d81b38296506dd000c0685cd
JMan <justinvang07@gmail.com>
2021-01-26 19:12:08 -0600

    created markdowns with to capture userResponse to readme file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,6 +15,39 @@ function generateMarkdown(userResponse) {
   return `
   <h1 align="center">${userResponse.title}</h1>
 
+  ![badge](https://img.shields.io/badge/license-${userResponse.license}-blue)<br />
+
+  ## Description
+  ${userResponse.description}
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#test)
+  - [Questions](#questions)
+
+  ## Installation
+  ${userResponse.installation}
+
+  ## Usage
+  ${userResponse.usage}
+
+  ## License
+  ${userResponse.license}
+  ![badge](https://img.shields.io/badge/license-${userResponse.license}-blue)
+
+  ## Contributing
+  ${userResponse.contributing}
+
+  ## Test
+  ${userResponse.test}
+
+  ## Questions
+  - Find me on GitHub: [${userResponse.username}](http://github.com/${userResponse.username})
+  - Email me with any questions: ${userResponse.email}
 `;
 }
 
